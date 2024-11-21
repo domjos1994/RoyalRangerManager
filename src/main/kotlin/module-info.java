f3e4f6c18f8ecc52472c1af4d02360a9e2942811
domjos <developing@domjos.de>
2024-11-21 07:17:14 +0100

    Initial commit

---
+++ src/main/kotlin/module-info.java
@@ -0,0 +1,16 @@
+module de.dojodev.royalrangermanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires kotlin.stdlib;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens de.dojodev.royalrangermanager to javafx.fxml;
+    exports de.dojodev.royalrangermanager;
+}
