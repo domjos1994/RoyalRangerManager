426d415419bd426efb2d729fdea00ad1ebc6b263
domjos <developing@domjos.de>
2024-12-16 18:00:56 +0100

    update

--- src/main/kotlin/de/dojodev/royalrangermanager/controller/UsersController.kt
+++ src/main/kotlin/de/dojodev/royalrangermanager/controller/UsersController.kt
@@ -7,7 +7,6 @@ import de.dojodev.royalrangermanager.repositories.UserRepository
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
-import javafx.collections.ObservableList
 import javafx.event.EventHandler
 import javafx.fxml.FXML
 import javafx.scene.control.TableColumn
@@ -20,14 +19,33 @@ import javafx.util.Callback
 
 class UsersController : SubController() {
 
-    @FXML private lateinit var tblUsers: TableView<User>
+    @FXML
+    private lateinit var tblUsers: TableView<User>
 
-    private var users = mutableListOf<User>()
     private var userRepository: UserRepository? = null
 
     override fun initControls() {
+        super.cmdNew?.isDisable = false
+        super.cmdEdit?.isDisable = true
+        super.cmdCancel?.isDisable = true
+        super.cmdSave?.isDisable = false
+        super.cmdDelete?.isDisable = true
+
+        val userModel = Project.get().getUser()
+
+        if(userModel != null) {
+            this.userRepository = UserRepository()
+            this.tblUsers.items.clear()
+
+            if(userModel.admin == 1 || userModel.trunkLeader == 1 || userModel.trunkWait == 1 || userModel.trunkHelper == 1) {
+                this.tblUsers.items.addAll(this.userRepository!!.getUsers())
+            } else {
+                this.tblUsers.items.add(userModel)
+            }
+        }
+
         super.cmdNew?.setOnAction {
-            this.users.add(User(0, "", "", 0, "", 0, 0, 0, 0, 0, 0))
+            this.tblUsers.items.add(User(0, "", "", 0, "", 0, 0, 0, 0, 0, 0))
         }
 
         val userNameCol = TableColumn<User, String>()
@@ -36,8 +54,8 @@ class UsersController : SubController() {
         userNameCol.cellFactory = TextFieldTableCell.forTableColumn()
         userNameCol.onEditCommit = EventHandler {
             try {
-                this.users[it.tablePosition.row].userName = it.newValue
-                this.userRepository!!.insertOrUpdate(this.users[it.tablePosition.row])
+                this.tblUsers.items[it.tablePosition.row].userName = it.newValue
+                this.userRepository!!.insertOrUpdate(this.tblUsers.items[it.tablePosition.row])
                 FXHelper.printSuccess()
             } catch (ex: Exception) {
                 FXHelper.printNotification(ex)
@@ -51,8 +69,8 @@ class UsersController : SubController() {
         emailCol.cellFactory = TextFieldTableCell.forTableColumn()
         emailCol.onEditCommit = EventHandler {
             try {
-                this.users[it.tablePosition.row].email = it.newValue
-                this.userRepository!!.insertOrUpdate(this.users[it.tablePosition.row])
+                this.tblUsers.items[it.tablePosition.row].email = it.newValue
+                this.userRepository!!.insertOrUpdate(this.tblUsers.items[it.tablePosition.row])
                 FXHelper.printSuccess()
             } catch (ex: Exception) {
                 FXHelper.printNotification(ex)
@@ -92,26 +110,72 @@ class UsersController : SubController() {
         adminCol.cellFactory = CheckBoxTableCell.forTableColumn(adminCol)
         this.tblUsers.columns.add(adminCol)
 
+        val trunkLeaderCol = TableColumn<User, Boolean>()
+        trunkLeaderCol.text = FXHelper.getBundle().getString("user.trunkLeader")
+        trunkLeaderCol.cellValueFactory = Callback { data ->
+            val user = data.value
+            val booleanProperty = SimpleBooleanProperty(user.trunkLeader == 1)
+            booleanProperty.addListener {_,_,new ->
+                user.trunkLeader = if(new) 1 else 0
+            }
+            booleanProperty
+        }
+        trunkLeaderCol.cellFactory = CheckBoxTableCell.forTableColumn(trunkLeaderCol)
+        this.tblUsers.columns.add(trunkLeaderCol)
+
+        val trunkWaitCol = TableColumn<User, Boolean>()
+        trunkWaitCol.text = FXHelper.getBundle().getString("user.trunkWait")
+        trunkWaitCol.cellValueFactory = Callback { data ->
+            val user = data.value
+            val booleanProperty = SimpleBooleanProperty(user.trunkWait == 1)
+            booleanProperty.addListener {_,_,new ->
+                user.trunkWait = if(new) 1 else 0
+            }
+            booleanProperty
+        }
+        trunkWaitCol.cellFactory = CheckBoxTableCell.forTableColumn(trunkWaitCol)
+        this.tblUsers.columns.add(trunkWaitCol)
+
+        val trunkHelperCol = TableColumn<User, Boolean>()
+        trunkHelperCol.text = FXHelper.getBundle().getString("user.trunkHelper")
+        trunkHelperCol.cellValueFactory = Callback { data ->
+            val user = data.value
+            val booleanProperty = SimpleBooleanProperty(user.trunkHelper == 1)
+            booleanProperty.addListener {_,_,new ->
+                user.trunkHelper = if(new) 1 else 0
+            }
+            booleanProperty
+        }
+        trunkHelperCol.cellFactory = CheckBoxTableCell.forTableColumn(trunkHelperCol)
+        this.tblUsers.columns.add(trunkHelperCol)
+
+        val leaderCol = TableColumn<User, Boolean>()
+        leaderCol.text = FXHelper.getBundle().getString("user.leader")
+        leaderCol.cellValueFactory = Callback { data ->
+            val user = data.value
+            val booleanProperty = SimpleBooleanProperty(user.leader == 1)
+            booleanProperty.addListener {_,_,new ->
+                user.leader = if(new) 1 else 0
+            }
+            booleanProperty
+        }
+        leaderCol.cellFactory = CheckBoxTableCell.forTableColumn(leaderCol)
+        this.tblUsers.columns.add(leaderCol)
+
+        val juniorLeaderCol = TableColumn<User, Boolean>()
+        juniorLeaderCol.text = FXHelper.getBundle().getString("user.juniorLeader")
+        juniorLeaderCol.cellValueFactory = Callback { data ->
+            val user = data.value
+            val booleanProperty = SimpleBooleanProperty(user.juniorLeader == 1)
+            booleanProperty.addListener {_,_,new ->
+                user.juniorLeader = if(new) 1 else 0
+            }
+            booleanProperty
+        }
+        juniorLeaderCol.cellFactory = CheckBoxTableCell.forTableColumn(juniorLeaderCol)
+        this.tblUsers.columns.add(juniorLeaderCol)
     }
 
     override fun init() {
-        super.cmdEdit?.isVisible = false
-        super.cmdCancel?.isVisible = false
-        super.cmdSave?.isVisible = false
-        super.cmdDelete?.isVisible = false
-
-        val user = Project.get().getUser()
-
-        if(user != null) {
-            this.userRepository = UserRepository()
-            this.users.clear()
-
-            if(user.admin == 1 || user.trunkLeader == 1 || user.trunkWait == 1 || user.trunkHelper == 1) {
-                users.addAll(this.userRepository!!.getUsers())
-            } else {
-                this.users.add(user)
-            }
-            this.tblUsers.items.addAll(this.users)
-        }
     }
 }
